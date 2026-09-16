53586455d2ed9025fefae37e0da97daad73d07f4
PROWLERx15 <prowlerx15@gmail.com>
2026-09-16 11:50:25 +0530

    feat: add Market Order Matching in Engine
    Signed-off-by: PROWLERx15 <prowlerx15@gmail.com>

--- src/exchange/core/engine.py
+++ src/exchange/core/engine.py
@@ -105,7 +105,80 @@ class MatchingEngine:
         return trades if len(trades) > 0 else None
 
     def _match_market_order(self, incoming_order: MarketOrder):
-        pass
+        trades = []
+
+        while incoming_order.quantity_remaining > 0:
+            if incoming_order.side == Side.BUY:
+                best_ask = self._orderbook.get_best_ask_price()
+
+                # No Asks -> Nothing to match
+                if best_ask is None:
+                    break
+                resting_order = self._orderbook.get_best_ask_order()
+                trade_price = best_ask
+            else:
+                best_bid = self._orderbook.get_best_bid_price()
+
+                # No Bids -> Nothing to match
+                if best_bid is None:
+                    break
+                resting_order = self._orderbook.get_best_bid_order()
+                trade_price = best_bid
+
+            if resting_order is not None:
+                trade_quantity = min(
+                    incoming_order.quantity_remaining, resting_order.quantity_remaining
+                )
+
+                # Update the Incoming Order
+                incoming_order.quantity_remaining -= trade_quantity
+                incoming_order.quantity_executed += trade_quantity
+
+                if incoming_order.quantity_remaining == 0:
+                    incoming_order.execution_status = ExecutionStatus.FILLED
+                    incoming_order.order_status = OrderStatus.COMPLETED
+                else:
+                    incoming_order.execution_status = ExecutionStatus.PARTIALLY_FILLED
+
+                # Update the Resting Order
+                resting_order.quantity_remaining -= trade_quantity
+                resting_order.quantity_executed += trade_quantity
+
+                # if the resting limit order is filled then pop it from the queue
+                if resting_order.quantity_remaining == 0:
+                    resting_order.order_status = OrderStatus.COMPLETED
+                    resting_order.execution_status = ExecutionStatus.FILLED
+                    self._orderbook.remove_order(resting_order.order_id)
+                else:
+                    resting_order.execution_status = ExecutionStatus.PARTIALLY_FILLED
+
+                buy_order_id = (
+                    incoming_order.order_id
+                    if incoming_order.side == Side.BUY
+                    else resting_order.order_id
+                )
+                sell_order_id = (
+                    incoming_order.order_id
+                    if incoming_order.side == Side.SELL
+                    else resting_order.order_id
+                )
+
+                trade = self._create_trade(
+                    buy_order_id,
+                    sell_order_id,
+                    trade_quantity,
+                    trade_price,
+                    timestamp=incoming_order.creation_timestamp,
+                )
+                trades.append(trade)
+
+        # Market Orders never rest in the orderbook
+        #  If there is quantity remaining, it means the opposite side of the
+        #  book is completely empty. The unfilled remainder is immediately expired.
+        if incoming_order.quantity_remaining > 0:
+            incoming_order.order_status = OrderStatus.EXPIRED
+
+        return trades if len(trades) > 0 else None
 
     def _create_trade(
         self,
